f0c89a33d9a31bc41bac2998245811d9f1b5ce7d
Collin <bauercl@g.cofc.edu>
2019-10-07 20:28:19 -0400

    working driver

--- TeamAutomation/project/org/opens/colorfinder/result/EqualsDriver.java
+++ TeamAutomation/project/org/opens/colorfinder/result/EqualsDriver.java
@@ -5,19 +5,77 @@ import org.opens.utils.contrastchecker.*;
 import java.awt.Color;
 
 public class EqualsDriver{
+    
+    /* Color values */
+    private static final int C_RED = 255;
+    private static final int C_GREEN = 0;
+    private static final int C_BLUE = 0;
+    /* Comparison color values */
+    private static final int CC_RED = 0;
+    private static final int CC_GREEN = 0;
+    private static final int CC_BLUE = 255;
+    /* Threshold value */
+    private static final double THRESHOLD = 50.0;
+
+    
     public static void main(String[] args){
-        ColorCombinaisonImpl test = new ColorCombinaisonImpl(new Color(255,0,0), new Color(0,0,255), 50.0);
-
-        System.out.println(test);
-
-        /*
+        String form = "Form: java test.EqualsDriver <Object Type> [arg1, arg2, ...]\n"
         if (args.length <= 0){
-            System.out.println("Arguments expected. Exiting.");
-            return;
+            System.out.printf("Arguments expected.\n" + form);
+            return; // exit program
         }
-        for(int i = 0; i < args.length; i++){
-            System.out.printf("\"%s\", ", args[i]);
+        
+        // initial object to test against
+        ColorCombinaisonImpl test = new ColorCombinaisonImpl(new Color(C_RED,C_GREEN,C_BLUE),
+                                    new Color(CC_RED,CC_GREEN,CC_BLUE), THRESHOLD);
+        // inital comparison object
+        Object comparison = null;
+
+        switch(args[0]){
+            case "null":
+                // Do nothing; object already null
+                break;
+            case "self":
+                comparison = test;
+                break;
+            case "Integer":
+                comparison = new Integer(args[1]);
+                break;
+            case "String":
+                comparison = new String(args[1]);
+                break;
+            case "ColorCombinaisonImpl":
+                // Case not fully built yet
+                comparison = new ColorCombinaisonImpl();
+                if (args.length > 1){
+                    int[] argNums = new int[6];
+                    double argThresh = 0;
+                    for (int i = 1; i < 7; i++){
+                        try{
+                            argNums[i + 2] = Integer.parseInt(args[i])
+                        }
+                        catch(Exception e){
+                            System.out.println("Arguments passed do not match expected." + form);
+                        }
+                    }
+                }
+                break;
+            default :
+                System.out.println("Object type not recognized by driver.");
+                return; // exit program
         }
-        */
+        
+        
+        
+        // declare test conditions in shell
+        System.out.printf("Comparing ColorCombinaisonImpl(Color(%d, %d, %d), Color(%d, %d, %d), %.1f) to %s: ",
+                          test.getColor().getRed(), test.getColor().getGreen(), test.getColor().getBlue(),
+                          test.getComparisonColor().getRed(), test.getComparisonColor().getGreen(),
+                          test.getComparisonColor().getBlue(), test.getThreshold(),
+                          comparison);
+
+        // print test results
+        System.out.printf("%s\n", test.equals(comparison));
+           
     }
 }
