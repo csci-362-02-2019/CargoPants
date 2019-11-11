be6aeba0aa26ee41dca41c9de63ca187bfc131e6
Isabel <ilally93@gmail.com>
2019-11-10 20:13:20 -0500

    Fixing files

---
+++ TestAutomation/scripts/runAllTests.sh
@@ -0,0 +1,30 @@
+# Primary testing script.
+# This script does multiple things:
+# 1. calls the compileDrivers helper script
+# 2. reads every test case into an array and
+#    passes each case individually to
+#    runSingleTest helper script
+
+# Script will only run from the TestAutomation
+#  directory. This helps keep things from
+#  breaking in unexpected ways.
+if [ ${PWD:len-14:14} != "TestAutomation" ]; then
+    echo "Script must be run from root directory."
+    exit
+fi
+
+# Compile drivers
+bash ./scripts/compileDrivers.sh
+echo
+
+# Instantiate and run test cases
+echo "Running test cases..."
+TESTCASES=./testCases/*
+for input in $TESTCASES
+do
+    if [ $input != "./testCases/testTemplate.txt" ]; then
+        bash ./scripts/runSingleTest.sh $input
+    fi
+done
+
+echo "Done."
